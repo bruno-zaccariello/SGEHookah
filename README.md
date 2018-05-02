42220844d804d66cfe48814b94d39bacdd6524eb
Bruno Zaccariello <brukzz90@gmail.com>
2018-05-02 13:04:17 -0300

    Delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# SGEHookah
-Repositório de desenvolvimento do sistema SGE Hookah para o cliente Rafael Prada em parceria com a Faculdade Impacta
--- core/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from core.models import Teste
-
-''' class TesteForm(forms.ModelForm):
-	class Meta:
-		model = Teste
-		fields = ['nome', 'descricao'] '''
