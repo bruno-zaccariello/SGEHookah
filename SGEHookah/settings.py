404b1081a1fbdd7b9b3eb0480c0107c9677c4883
Bruno Zaccariello <30813635+SpaceTheArcher@users.noreply.github.com>
2018-06-18 11:27:47 -0300

    Update settings.py

--- SGEHookah/settings.py
+++ SGEHookah/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'SGEHookah.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}'''
+}
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'sql_server.pyodbc',
         'NAME': 'endoramadb',
@@ -90,7 +90,7 @@ DATABASES = {
         'HOST': 'endorama.database.windows.net',
         'PORT': '',
     }
-}
+}'''
 
 
 # Password validation
