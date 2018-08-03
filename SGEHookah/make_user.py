befb4c58b44383c59c2eb9d61c6b21edc0eb579d
Bruno Zaccariello <brukzz90@gmail.com>
2018-08-03 14:18:27 -0300

    Delete Unused File

--- SGEHookah/make_user.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-
-if User.objects.get(email='admin@example.com'):
-    pass
-else:
-    User.objects.create_superuser('admin', 'admin@example.com', 'pass')
